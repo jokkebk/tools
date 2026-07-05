8f8f0588b1f1fead2953d9d417b5e1b2eb25f36a
Joonas Pihlajamaa <joonas.pihlajamaa@iki.fi>
2026-07-05 09:37:05 +0300

    Add mobile PWA support for scribe dictation

---
+++ scribe_dictation-sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "scribe-dictation-v1";
+const APP_SHELL = [
+  "scribe_dictation.html",
+  "scribe_dictation-manifest.json",
+  "scribe_dictation-icon.svg",
+  "scribe_dictation-icon-192.png",
+  "scribe_dictation-icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys
+        .filter((key) => key !== CACHE_NAME)
+        .map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== location.origin || event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => (
+      cached || fetch(event.request).then((response) => {
+        if (response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      })
+    ))
+  );
+});
+
